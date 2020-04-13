67ea72831912112ae738de47abf1f0fdeca2ddd7
Mariya <mariya.pososinova@gmail.com>
2020-04-13 11:47:28 +0100

    Added modules(project structure)

---
+++ src/components/app-header/app-header.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const AppHeader = () => {
+  return (
+    <div className="app-header d-flex">
+      <h1>My ToDo List</h1>
+      <h2>5 items, liked 0</h2>
+    </div>
+  );
+};
+
+export default AppHeader;
