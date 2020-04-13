d37276781a44c799460c64cd4b88b8003ef90a08
Mariya <mariya.pososinova@gmail.com>
2020-04-13 12:34:00 +0100

    buttons all & liked

--- src/components/post-status-filter/post-status-filter.js
+++ src/components/post-status-filter/post-status-filter.js
@@ -5,8 +5,12 @@ import "./post-status-filter.css";
 const PostStatusFilter = () => {
   return (
     <div className="btn-group">
-      <button />
-      <button />
+      <button className="btn btn-info" type="button">
+        All
+      </button>
+      <button className="btn btn-outline-secondary" type="button">
+        Liked
+      </button>
     </div>
   );
 };
