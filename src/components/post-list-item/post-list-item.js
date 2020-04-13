b18fdc947109d7982978f6269cf1fee37a2e90a4
Mariya <mariya.pososinova@gmail.com>
2020-04-13 16:09:12 +0100

    click on star functionality

--- src/components/post-list-item/post-list-item.js
+++ src/components/post-list-item/post-list-item.js
@@ -1,27 +1,47 @@
-import React from "react";
+import React, { Component } from "react";
 
 import "./post-list-item.css";
 
-const PostListItem = ({ label, important = false }) => {
-  let classNames = "app-list-item d-flex justify-content-between";
-  if (important) {
-    classNames += " important";
+export default class PostListItem extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      important: false,
+    };
+    this.onImportant = this.onImportant.bind(this);
   }
 
-  return (
-    <div className={classNames}>
-      <span className="app-list-item-label">{label}</span>
-      <div className="d-flex justify-content-center align-items-center">
-        <button className="btn-star btn-sm" type="button">
-          <i className="fa fa-star"></i>
-        </button>
-        <button className="btn-trash btn-sm" type="button">
-          <i className="fa fa-trash-o"></i>
-        </button>
-        <i className="fa fa-heart"></i>
-      </div>
-    </div>
-  );
-};
+  onImportant() {
+    this.setState(({ important }) => ({
+      important: !important,
+    }));
+  }
 
-export default PostListItem;
+  render() {
+    const { label } = this.props;
+    const { important } = this.state;
+
+    let classNames = "app-list-item d-flex justify-content-between";
+    if (important) {
+      classNames += " important";
+    }
+    return (
+      <div className={classNames}>
+        <span className="app-list-item-label">{label}</span>
+        <div className="d-flex justify-content-center align-items-center">
+          <button
+            className="btn-star btn-sm"
+            type="button"
+            onClick={this.onImportant}
+          >
+            <i className="fa fa-star"></i>
+          </button>
+          <button className="btn-trash btn-sm" type="button">
+            <i className="fa fa-trash-o"></i>
+          </button>
+          <i className="fa fa-heart"></i>
+        </div>
+      </div>
+    );
+  }
+}
